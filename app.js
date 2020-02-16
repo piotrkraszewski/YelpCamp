0b130d5a07133e832556e6a99237c88ac12606b9
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-02-16 18:57:49 +0100

    enviroment viarable for Port  in app.listen added

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ var express = require('express'),
 	Campground = require('./models/campground.js'),
 	Comment = require('./models/comment.js'),
 	User = require('./models/user.js'),
-	passport = require("passport"),
-	LocalStrategy = require("passport-local"),
-	User = require("./models/user"),
-	methodOverride = require("method-override"),
-	flash = require("connect-flash"),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local'),
+	User = require('./models/user'),
+	methodOverride = require('method-override'),
+	flash = require('connect-flash'),
 	seedDB = require('./seeds.js');
 
 // requireing routes
-var commentRoutes = require("./routes/comments"),
-	campgroundRoutes = require("./routes/campgrounds"),
-	indexRoutes = require("./routes/index")
-
+var commentRoutes = require('./routes/comments'),
+	campgroundRoutes = require('./routes/campgrounds'),
+	indexRoutes = require('./routes/index');
 
 // seedDB(); // seed the data
 
@@ -25,54 +24,53 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-
 // mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
+var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
 
-mongoose.connect(url, { useNewUrlParser: true,
-useCreateIndex: true
-}).then(() =>{
-	console.log('Connected to DB!')
-}).catch(err => {
-	console.log('ERROR:', err.message)
-})
-
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useCreateIndex: true
+	})
+	.then(() => {
+		console.log('Connected to DB!');
+	})
+	.catch((err) => {
+		console.log('ERROR:', err.message);
+	});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"))
-app.use(flash())
-
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(flash());
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "lol",
-	resave: false,
-	saveUninitialized: false
-}))
+app.use(
+	require('express-session')({
+		secret: 'lol',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user
-	res.locals.error = req.flash("error")
-	res.locals.success = req.flash("success")
-	next()
-})
-
-app.use(indexRoutes)
-app.use("/campgrounds", campgroundRoutes)
-app.use("/campgrounds/:id/comments", commentRoutes)
-
-// to musi byæ ¿eby dzia³a³o ne heroku
-// app.listen(process.env.PORT, process.env.IP);
-
-app.listen(3000, function() {
-	console.log('Server running on port 3000');
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
+	next();
 });
+
+app.use(indexRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
+
+// to musi byæ ¿eby dzia³a³o na heroku i na pc w port 3000
+app.listen(process.env.PORT, process.env.IP);
